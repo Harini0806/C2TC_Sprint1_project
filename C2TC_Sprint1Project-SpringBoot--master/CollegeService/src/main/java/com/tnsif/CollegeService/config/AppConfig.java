242690744ab7fea5e0117e4e8b436a66669a886f
Harini0806 <142818688+Harini0806@users.noreply.github.com>
2025-11-10 09:57:12 +0530

    Delete C2TC_Sprint1Project-SpringBoot--master/CollegeService/src directory

--- C2TC_Sprint1Project-SpringBoot--master/CollegeService/src/main/java/com/tnsif/CollegeService/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.tnsif.CollegeService.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class AppConfig {
-    // You can define Beans or CORS config here if needed
-}
-
--- C2TC_Sprint1Project-SpringBoot--master/CollegeService/src/main/java/com/tnsif/CollegeService/repository/CollegeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tnsif.CollegeService.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.tnsif.CollegeService.entity.College;
-
-public interface CollegeRepository extends JpaRepository<College, Integer> {
-    // JPA already provides CRUD methods: findAll, findById, save, deleteById
-}
-
